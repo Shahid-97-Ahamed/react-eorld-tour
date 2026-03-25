6d474beda1f28bce7d7ea572964830cd06ecf2e7
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-03-25 19:43:56 +0900

    error handiling css set

--- world-tour/src/component/countries.jsx
+++ world-tour/src/component/countries.jsx
@@ -1,13 +1,20 @@
 import React, { use } from 'react';
+import Country from './country/country';
+import './countries.css'
 
 const Countries = ({ countriesPromise }) => {
     const countryData =use(countriesPromise);
     // console.log(countryData);   
     const shownCountries = countryData.countries;
-    console.log(shownCountries);
+    // console.log(shownCountries);
     return (
         <div>
             <h3>Countries Size :{shownCountries.length}</h3>
+           <div className='countries'>
+             {
+                shownCountries.map(country => <Country key={country.ccn3.ccn3} country={country} />)
+            }
+           </div>
         </div>
     );
 };
---
+++ world-tour/src/component/country/country.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './country.css'
+
+const Country = ({country}) => {
+    // console.log(country.continents.name);
+    const [showInfo, setShowInfo] = useState(true);
+    const btnhandele=()=>{
+        setShowInfo(!showInfo);
+    }
+  
+    return (
+        <div className={`country ${showInfo ? 'country-clicked btn2' : ''}`}>
+            <img src={country.flags.flags.png} alt={country.name.common} />
+            <h3>name: {country.name.common}</h3>
+            <h4>Population: {country.population.population}</h4>
+            <h5>Language:{country.languages.languages.eng} {country.languages.languages.eng === "English" ? "" : "Other Language"}</h5>
+            <button className={"btn "} onClick={btnhandele}>
+                {showInfo ? "Checked" : "Unchecked"}
+            </button>
+        </div>
+    );
+};
+
+export default Country;
